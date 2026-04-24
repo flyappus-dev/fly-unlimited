1409f326e638806efb991636c12b2c1cda587e0c
flyappus-dev <flyappus@gmail.com>
2026-04-23 20:19:58 -0400

    Update site URL and add redirects in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
+	site: "https://fly-unlimited.com",
+	redirects: {
+		"/join": "https://apps.apple.com/app/id6758623880",
+	},
 	integrations: [mdx(), sitemap()],
 	adapter: cloudflare({
 		platformProxy: {
